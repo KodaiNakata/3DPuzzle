cd685ee4fdcc9e3bccb60d16a508e92089f9a50a
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-07-16 23:25:08 +0900

    カメラ移動(#6)を追加

---
+++ 3DPuzzle/Assets/Project/Program/GameScene/Script/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ * @file CameraController.cs
+ * @brief カメラの移動を扱うファイル
+ * @author Kodai Nakata
+ */
+
+/**
+ * @class CameraController
+ * @brief カメラの移動を扱うファイル
+ */
+public class CameraController : MonoBehaviour
+{
+    private const float ROTATE_SPEED = 3.0f;// 回転する速さ
+    private Vector3 centerRotation = new Vector3(20.0f,0.0f,20.0f);// 回転の中心座標
+
+    /**
+     * @brief 1フレームごとに呼び出される関数
+     */
+    void Update()
+    {
+        // Shiftキーが押されている間
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            float angle = Input.GetAxis("Horizontal") * ROTATE_SPEED;// 回転させる角度(左右方向キーが押されている)
+
+            transform.RotateAround(centerRotation, Vector3.down, angle);// y軸を中心に回転させる
+        }
+        
+    }
+}
