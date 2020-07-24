0ac9ad32e4f978a3e5a352a094b319a11eed475d
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-07-25 00:57:08 +0900

    ゲームオーバー画面からスタート画面への遷移を追加(#11)。

---
+++ 3DPuzzle/Assets/Project/Program/GameOverScene/Script/GameOverDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/**
+* @file GameOverDirector.cs
+* @brief ゲームオーバー画面の監督スクリプトのファイル
+* @author Kodai Nakata
+*/
+
+/**
+ * @class GameOverDirector
+ * @brief ゲームオーバー画面の監督スクリプト用のクラス
+ */
+public class GameOverDirector : MonoBehaviour
+{
+    /**
+     * @brief 1フレームごとに呼び出される関数
+     */
+    void Update()
+    {
+        // エンターキーが押されたとき
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("StartScene");// スタート画面へ遷移
+        }
+        
+    }
+}
