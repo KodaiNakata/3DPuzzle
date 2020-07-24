c8672328ae839f656e425132239cecc26ad9ce76
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2020-07-25 00:55:47 +0900

    スタート画面からゲーム画面への遷移を追加(#10)。

---
+++ 3DPuzzle/Assets/Project/Program/StartScene/Script/StartSceneDirector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/**
+* @file StartSceneDirector.cs
+* @brief スタート画面の監督スクリプトのファイル
+* @author Kodai Nakata
+*/
+
+/**
+ * @class StartSceneDirector
+ * @brief スタート画面の監督スクリプト用のクラス
+ */
+public class StartSceneDirector : MonoBehaviour
+{
+    /**
+     * @brief 1フレームごとに呼び出される関数
+     */
+    void Update()
+    {
+        // エンターキーが押されたとき
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneManager.LoadScene("GameScene");// ゲーム画面へ遷移
+        }
+    }
+}
